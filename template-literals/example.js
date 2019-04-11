10da50dc0bd7245c36d9c2b47211bfdef3225be4
iganbold <itgel.ganbold1@gmail.com>
2019-04-11 18:30:35 -0400

    Test ES6 template literals

---
+++ template-literals/example.js
@@ -0,0 +1,11 @@
+let word1 = "Itgel";
+let word2 = "Ganbold";
+let num1 = 10;
+let num2 = 11;
+
+let fullName = `Fullname: ${word1} ${word2} \nAge: ${num1 + num2}`
+
+let multiLineTab = `    ${word1}
+    ${word2}`;
+console.log(fullName);
+console.log(multiLineTab);
