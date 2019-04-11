7ba3daa4d5de86e299840d16f9e8303cbe9ba3cd
iganbold <itgel.ganbold1@gmail.com>
2019-04-11 18:46:15 -0400

    Test ES6 destructuring array

---
+++ destructure-arrays/index.js
@@ -0,0 +1,8 @@
+// refrence each index by name
+// example: fn -> [0]
+let [fn, mn, ln] = ['Itgel', 'Coding God', 'Ganbold'];
+console.log(`${fn} ${mn}`);
+
+// Change the array's [0] index value to Bat-Itgel
+fn = 'Bat-Itgel';
+console.log(`${fn} ${ln}`);
